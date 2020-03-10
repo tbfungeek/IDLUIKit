7a180cba18f2c183a1fe2d865407f8235211773a
linxiaohai <tbfungeek@163.com>
2020-03-10 16:21:21 +0800

    [-]删除pod 目录

--- Example/Pods/Target Support Files/IDLUIKit/IDLUIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IDLUIKit : NSObject
-@end
-@implementation PodsDummy_IDLUIKit
-@end
--- Example/Pods/Target Support Files/Pods-IDLUIKit_Example/Pods-IDLUIKit_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IDLUIKit_Example : NSObject
-@end
-@implementation PodsDummy_Pods_IDLUIKit_Example
-@end
